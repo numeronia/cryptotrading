8ae1752b14c9fa9afb9470cb13c35df258a28213
Darien Chua <chua.darien@gmail.com>
2023-11-07 21:13:39 +0800

    Amended API gateways

--- src/main/java/com/cryptotrade/controller/TradingController.java
+++ src/main/java/com/cryptotrade/controller/TradingController.java
@@ -17,9 +17,17 @@ public class TradingController {
     private TradingService tradingService;
 
     @GetMapping("/price/{currencyPair}")
-    public ResponseEntity<Price> getLatestBestPrice(@PathVariable String currencyPair) {
-        Price price = tradingService.getLatestBestPrice(currencyPair);
-        return ResponseEntity.ok(price);
+    public ResponseEntity<?> getLatestBestPrice(@PathVariable String currencyPair) {
+        List<Price> prices = tradingService.getAggregatedPrices(currencyPair);
+        if (currencyPair.equalsIgnoreCase("BTCUSDT")) {
+            Price price = prices.get(0);
+            String str = ("Bid: " + price.getBidPrice() + " ,Ask: " + price.getAskPrice());
+            return ResponseEntity.ok(str);
+        } else {
+            Price price = prices.get(1);
+            String str = ("Bid: " + price.getBidPrice() + " ,Ask: " + price.getAskPrice());
+            return ResponseEntity.ok(str);
+        }
     }
 
     @PostMapping("/trade")
@@ -38,14 +46,16 @@ public class TradingController {
     public ResponseEntity<?> getWalletBalance(@RequestParam String userId) {
         try {
             Wallet wallet = walletService.getWalletByUserId(userId);
-            return ResponseEntity.ok(wallet.getBalances());
+            List<WalletBalance> balances = wallet.getBalances();
+            String str = ("USDT: " + balances.get(0).getBalance() + " ,BTC: " + balances.get(1).getBalance() + " ,ETH: " + balances.get(2).getBalance());
+            return ResponseEntity.ok(str);
         } catch (Exception e) {
             // Log and handle the exception appropriately
             return ResponseEntity.badRequest().body("Error retrieving wallet balance");
         }
     }
 
-    @GetMapping("/history")
+    @GetMapping("/tradingHistory")
     public ResponseEntity<?> getTradingHistory(@RequestParam String userId) {
         try {
             List<Transaction> transactions = transactionHistoryService.getTransactionsForUser(userId);
